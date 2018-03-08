16be6f1950f55627a706da992db5439749237985
Inzert <31589147+Inzert@users.noreply.github.com>
2018-03-08 21:55:08 +0800

    Add files via upload

---
+++ sphereController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sphereController : MonoBehaviour {
+    private Rigidbody rb;
+    public int speed;
+
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float horiInput = Input.GetAxis("Horizontal");
+         float vertInput =Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horiInput, 0, vertInput);
+        rb.AddForce(movement * speed);
+       
+    }
+   
+   
+
+        
+        
+   
+    
+}
+
